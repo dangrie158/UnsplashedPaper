003fb7300f6ae6d4882b3734debce12b71243db1
Daniel Grießhaber <dangrie158@gmail.com>
2020-03-13 19:47:53 +0100

    Initial Commit

--- UnsplashedPaper/AppDelegate.swift
+++ UnsplashedPaper/AppDelegate.swift
@@ -13,21 +13,157 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
+    var statusBarItem: NSStatusItem!
+    var timer: Timer!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
+        
+        let statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+        
+        statusBarItem.button?.image = NSImage(named: "MenuIcon")
+        statusBarItem.button?.image?.isTemplate = true
+        
+        let statusBarMenu = NSMenu(title: "UnsplashedPaper")
+        
+        statusBarItem.menu = statusBarMenu
+        let preferencesItem = NSMenuItem(
+            title: "Preferences",
+            action: #selector(AppDelegate.showPreferences),
+            keyEquivalent: ",")
+        preferencesItem.keyEquivalentModifierMask = .command
+        
+        let updateNowItem = NSMenuItem(
+            title: "Refresh Now",
+            action: #selector(AppDelegate.updateWallpaper),
+            keyEquivalent: "R")
+        updateNowItem.keyEquivalentModifierMask = .command
+        
+        let quitItem = NSMenuItem(
+            title: "Quit",
+            action: #selector(NSApplication.shared.terminate),
+            keyEquivalent: "")
+        
+        statusBarMenu.addItem(preferencesItem)
+        statusBarMenu.addItem(updateNowItem)
+        statusBarMenu.addItem(NSMenuItem.separator())
+        statusBarMenu.addItem(quitItem)
+        
+        updateWallpaper()
+        scheduleUpdateJob()
+    }
+    
+    func scheduleUpdateJob() {
+        let defaults = UserDefaults.standard
+        
+        let updateInterval = defaults.object(forKey: "updateInterval") as? Int ?? 60
+        timer = Timer(timeInterval: TimeInterval(updateInterval), target: self, selector: #selector(AppDelegate.updateWallpaper), userInfo: nil, repeats: false)
+        
+        
+        RunLoop.main.add(timer, forMode: .common)
+    }
+    
+    func getRandomWallpaper(withSize size: NSSize, fromCollection: String? = nil, forSearchterm: String? = nil, completion: @escaping (_ result: URL)->()) {
+        let temporaryDirectoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+        let temporaryFilename = UUID().uuidString
+        let destinationFileUrl = temporaryDirectoryURL.appendingPathComponent(temporaryFilename)
+        
+        var fileUrlComponents = URLComponents(string: "https://source.unsplash.com")!
+        if let collection = fromCollection, !collection.isEmpty {
+            fileUrlComponents.path.append("/collection/\(collection)")
+        }else{
+            fileUrlComponents.path.append("/random")
+        }
+        
+        fileUrlComponents.path.append("/\(Int(size.width))x\(Int(size.height))/")
+        
+        if let query = forSearchterm, !query.isEmpty {
+            fileUrlComponents.query = query
+        }
+        
+        let sourceFileUrl = fileUrlComponents.url!
+        print("Downloading image from: \(sourceFileUrl)")
+           
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
 
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        let request = URLRequest(url: sourceFileUrl)
+
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // Success
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    print("Successfully downloaded. Status code: \(statusCode)")
+                }
+
+                do {
+                    try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                    completion(destinationFileUrl)
+                } catch (let writeError) {
+                    print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                }
+
+            } else {
+                print("Error took place while downloading a file. Error description: %@", error!);
+            }
+        }
+        task.resume()
+        
+    }
+    
+    @objc
+    func updateWallpaper() {
+        let defaults = UserDefaults.standard
+        let scaleImages = defaults.object(forKey: "scaleImages") as? Bool ?? true
+        let imagePerScreen = defaults.object(forKey: "imagePerScreen") as? Bool ?? true
+        let collectionId = defaults.object(forKey: "collectionId") as? String
+        let searchQuery = defaults.object(forKey: "searchQuery") as? String
+        
+        do {
+            let workspace = NSWorkspace.shared
+
+            if (imagePerScreen) {
+                for screen in NSScreen.screens {
+                    let screenSize = screen.visibleFrame.size
+                    getRandomWallpaper(withSize: screenSize, fromCollection: collectionId, forSearchterm: searchQuery) { url in
+                        var screenOptions = workspace.desktopImageOptions(for: screen)!
+                        screenOptions[NSWorkspace.DesktopImageOptionKey.imageScaling] = scaleImages
+                        do {
+                            try workspace.setDesktopImageURL(url, for: screen, options: screenOptions)
+                        } catch {
+                            NSLog("\(error)")
+                        }
+                    }
+                }
+            } else {
+                
+                let maxWidth = NSScreen.screens.map{$0.visibleFrame.width}.max()!
+                let maxHeight = NSScreen.screens.map{$0.visibleFrame.height}.max()!
+                let size =  NSSize(width: maxWidth, height: maxHeight)
+                    
+                getRandomWallpaper(withSize: size, fromCollection: collectionId, forSearchterm: searchQuery) { url in
+                    for screen in NSScreen.screens {
+                        var screenOptions = workspace.desktopImageOptions(for: screen)!
+                        screenOptions[NSWorkspace.DesktopImageOptionKey.imageScaling] = scaleImages
+                        do {
+                            try workspace.setDesktopImageURL(url, for: screen, options: screenOptions)
+                        } catch {
+                            NSLog("\(error)")
+                        }
+                    }
+                }
+            }
+        }
+        scheduleUpdateJob()
+    }
+    
+    
+    @objc
+    func showPreferences() {
+        let storyBoard = NSStoryboard(name:"Main", bundle:nil);
+        let controller = storyBoard.instantiateController(withIdentifier: "PreferencesController") as! NSWindowController
+        controller.showWindow(self)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ UnsplashedPaper/PreferencesView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  UnsplashedPaper
+//
+//  Created by Daniel Grießhaber on 12.03.20.
+//  Copyright © 2020 Daniel Grießhaber. All rights reserved.
+//
+import AppKit
+
+class Preferences: NSViewController {
+    @IBOutlet weak var updateInterval: NSTextFieldCell!
+    @IBOutlet weak var scaleImages: NSButton!
+    @IBOutlet weak var seperateImages: NSButton!
+    @IBOutlet weak var collection: NSTextField!
+    @IBOutlet weak var queryString: NSTokenField!
+    
+    override func viewDidLoad() {
+        let defaults = UserDefaults.standard
+        scaleImages.state = NSControl.StateValue.init(defaults.bool(forKey: "scaleImages") ? 1 : 0)
+        seperateImages.state = NSControl.StateValue.init(defaults.bool(forKey: "imagePerScreen") ? 1 : 0)
+        collection.stringValue = defaults.string(forKey: "collectionId") ?? ""
+        queryString.stringValue = defaults.string(forKey: "searchQuery") ?? ""
+        let savedUpdateInterval = Int32(defaults.integer(forKey: "updateInterval"))
+        updateInterval.intValue = savedUpdateInterval != 0 ? savedUpdateInterval : 60
+    }
+    
+    
+    @IBAction func settingChanged(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(scaleImages.state.rawValue == 1, forKey: "scaleImages")
+        defaults.set(seperateImages.state.rawValue == 1, forKey: "imagePerScreen")
+        defaults.set(collection.stringValue, forKey: "collectionId")
+        defaults.set(queryString.stringValue, forKey: "searchQuery")
+        print(queryString.stringValue)
+        defaults.set(updateInterval.intValue, forKey: "updateInterval")
+    }
+    
+    @IBAction func quitApp(_ sender: Any) {
+        NSApp.terminate(self)
+    }
+    
+    @IBAction func refreshNow(_ sender: Any) {
+        (NSApp.delegate as! AppDelegate).updateWallpaper()
+    }
+}
